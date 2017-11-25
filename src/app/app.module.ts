a5ac02ee4234221f43becc7b678e99abcbc22611
carloscalderon <cdcalderon@gmail.com>
2017-11-25 02:31:07 -0600

    Implement search Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 // Routes
 import {APP_ROUTING} from './app.routes';
+
+// Services
+import {SpotifyService} from './services/spotify.service';
+
+// Pipes
+import { NopicturePipe } from './pipes/nopicture.pipe';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -11,18 +19,26 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 
 
+
+
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     SearchComponent,
-    NavbarComponent
+    NavbarComponent,
+    NopicturePipe
   ],
   imports: [
     BrowserModule,
-    APP_ROUTING
+    APP_ROUTING,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    SpotifyService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
